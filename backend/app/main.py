1d76a12fc886e5787a0cf7d7746bac1153076048
Saamehada <dantebelieber.jb@gmail.com>
2026-05-22 12:31:12 +0530

    fix: opened CORS to allow Vercel connections

--- backend/app/main.py
+++ backend/app/main.py
@@ -55,7 +55,7 @@ app.add_exception_handler(Exception, general_exception_handler)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=settings.BACKEND_CORS_ORIGINS,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
